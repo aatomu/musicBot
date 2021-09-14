4cc4f0326701aa555d52d8fc9fed9951f44d9af4
atomu21263 <atom.aizu@gmail.com>
2021-09-14 20:05:00 +0900

    delete

--- start.sh
+++
@@ -1,4 +0,0 @@
-screen -r music -X kill
-screen -U -A -md -S music
-screen -r music -X stuff "cd $(dirname $0)/\n"
-screen -r music -X stuff "while :; do go run main.go -prefix=\!music -token=ODcyMzU5MTI3MTgyMDk0Mzc3.YQotvw.i4_00VV9GSCsd_ypn1I3R-CfwJU ; done\n"
